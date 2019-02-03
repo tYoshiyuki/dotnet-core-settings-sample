a4a2f67111b3ec776409da62ce797cfa576cb8ab
yTsunomoto <wasiwasimaru@gmail.com>
2019-02-03 16:51:20 +0900

    update

---
+++ DotNetCoreSettings/Configs/AppConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCoreSettings.Configs
+{
+    public class AppConfig
+    {
+        public Storage Storage { get; set; }
+    }
+
+    public class Storage
+    {
+        public int TimeOut { get; set; }
+        public BlobSettings[] Blobs { get; set; }
+    }
+
+    public class BlobSettings
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ DotNetCoreSettings/Program.cs
@@ -0,0 +1,31 @@
+﻿using DotNetCoreSettings.Configs;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace DotNetCoreSettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 設定ファイルの読み込み
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appconfig.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            // 設定値の取得
+            var appConfig = new AppConfig();
+            config.Bind(appConfig);
+
+            // 取得した設定値の確認
+            Console.WriteLine("TimeOut:" + appConfig.Storage.TimeOut);
+            foreach(var blob in appConfig.Storage.Blobs)
+            {
+                Console.WriteLine("Name:" + blob.Name);
+                Console.WriteLine("Url:" + blob.Url);
+            }
+        }
+    }
+}
